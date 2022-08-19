8266c517a87960c87690639c8f7a009af8fdb291
amosmaundundu <amosmaundu5@gmail.com>
2022-08-19 21:19:11 +0300

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Code your solution in this file!
+const scuberHeadquarter = 42;
+function distanceFromHqInBlocks(pickUpLocation){
+    const distanceInBlocks=pickUpLocation>scuberHeadquarter? pickUpLocation-scuberHeadquarter:scuberHeadquarter-pickUpLocation;
+    return distanceInBlocks;
+
+} 
+//return distance in feet
+function distanceFromHqInFeet(pickUpLocation){
+    let distanceInBlocks=distanceFromHqInBlocks(pickUpLocation);
+    return distanceInBlocks*264;
+}
+//distance travelled in feet given starting block and ending block
+function distanceTravelledInFeet(startingBlock, endingBlock){
+    let distanceTravelled= endingBlock>startingBlock?(endingBlock-startingBlock)*264: (startingBlock-endingBlock)*264;
+    return distanceTravelled;
+}
+
+//customer return fare
+function calculatesFarePrice(startingBlock,endingBlock){
+    let distanceInFeet = distanceTravelledInFeet(startingBlock,endingBlock);
+    if (distanceInFeet<=400){
+    return 0;
+}
+    else if(distanceInFeet >400 && distanceInFeet < 2000){
+    return (distanceInFeet-200)*0.02;
+}
+else if(distanceInFeet2000){
+    if(distanceInFeet>2500){
+        return "cannot travel that far";
+    }
+    return 25;
+}
+}
+
